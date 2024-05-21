681995d53892b7cfe82a6ff16ae7dfffcdbe55c5
M. AsadUllah <68208088+m-asadullah@users.noreply.github.com>
2024-05-21 02:20:28 -0700

    list all files

--- src/main/java/com/cubixos/ManageFiles.java
+++ src/main/java/com/cubixos/ManageFiles.java
@@ -1,2 +1,34 @@
-package com.cubixos.manage;public class ManageFiles {
+package com.cubixos;
+
+import java.io.File;
+import java.util.*;
+
+/**
+ * The core public access point to the manage-files functionality.
+ *
+ * @author Cubixos
+ */
+
+public class ManageFiles {
+
+    private ManageFiles() {
+    }
+
+    public static List<File> listFiles(String directory) {
+        Set<File> fileSet = new HashSet<>();
+        if (directory != null) {
+            File folder = new File(directory);
+            if (folder.exists()) {
+                if (folder.isDirectory()) {
+                    if (Objects.requireNonNull(folder.listFiles()).length > 0) {
+                        fileSet.addAll(Arrays.asList(folder.listFiles()));
+                    }
+                }
+            }
+        }
+        List<File> fileList = new ArrayList<>(fileSet);
+        Collections.sort(fileList);
+        return fileList;
+    }
+
 }
